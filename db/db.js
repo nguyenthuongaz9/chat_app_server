e552c16be7e27a12be7dc30767d486d838e1810f
nguyen thuong <nguyenthuongaz90@gmail.com>
2024-10-26 00:35:28 +0700

    fixed error mongo db url

--- db/db.js
+++ db/db.js
@@ -3,7 +3,13 @@ import mongoose from "mongoose";
 
 const connectDB = async ()=> {
     try {
-        const conn = await mongoose.connect(process.env.DATABASE_URL);
+        const conn = await mongoose.connect(process.env.DATABASE_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000, // Thời gian chờ cho việc chọn server
+            socketTimeoutMS: 45000 // Thời gian chờ cho socket
+        });
+        
         console.log(`MongoDB Connected: ${conn.connection.host}`);       
     } catch (error) {
         console.error(error);
