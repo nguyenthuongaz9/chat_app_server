19865ac5fd57e08f23ae857fe75fbe4e5c06b9b6
nguyen thuong <nguyenthuongaz90@gmail.com>
2024-10-25 23:39:47 +0700

    update cors origin

--- index.js
+++ index.js
@@ -18,9 +18,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(cors({
-    origin: (origin, callback) => {
-        callback(null, true);
-    },
+    origin: 'https://chat-app-akj6.onrender.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
 }));
@@ -32,7 +30,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://chat-app-akj6.onrender.com",
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
         credentials: true,
     }
